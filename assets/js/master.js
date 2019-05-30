787c8df49c4f05aaaaed956b00b7ae19f3deedab
tamisko0308 <48802325+tamisko0308@users.noreply.github.com>
2019-05-30 13:14:04 +0300

    Update contact form

--- assets/js/master.js
+++ assets/js/master.js
@@ -379,34 +379,36 @@
 
     /* ===== CONTACT FORM ===== */
 	 
-	$(function () {
+	// $(function () {
 
-    $('#contact-form').validator();
+  //   $('#contact-form').validator();
 
-		$('#contact-form').on('submit', function (e) {
-			if (!e.isDefaultPrevented()) {
-				var url = "assets/php/contact.php";
+	// 	$('#contact-form').on('submit', function (e) {
+	// 		if (!e.isDefaultPrevented()) {
+	// 			e.preventDefault();	
+	// 			$.ajax({
+	// 				type: $(this).attr("method") || "POST",
+	// 				url: $(this).attr("action"),
+	// 				data: $(this).serialize(),
+	// 				success: function (data)
+	// 				{
+	// 					var messageAlert = 'alert-' + data.type;
+	// 					var messageText = data.message;
 	
-				$.ajax({
-					type: "POST",
-					url: url,
-					data: $(this).serialize(),
-					success: function (data)
-					{
-						var messageAlert = 'alert-' + data.type;
-						var messageText = data.message;
-	
-						var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-						if (messageAlert && messageText) {
-							$('#contact-form').find('.messages').html(alertBox);
-							$('#contact-form')[0].reset();
-						}
-					}
-				});
-				return false;
-			}
-		});
-	});
+	// 					var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+	// 					if (messageAlert && messageText) {
+	// 						$('#contact-form').find('.messages').html(alertBox);
+	// 						$('#contact-form')[0].reset();
+	// 					}
+	// 				},
+	// 				error: function (jXHR, textStatus, errorThrown) {
+	// 						alert(errorThrown);
+	// 				}
+	// 			});
+	// 			return false;
+	// 		}
+	// 	});
+	// });
 
     /* ===== CBP PORTFOLIO ===== */
 	 $(window).load(function(){
